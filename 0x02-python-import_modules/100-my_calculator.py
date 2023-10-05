f681f5377bda624f31686a43caaed878b266526e
SalahMSwefy <salahm.swefy@gmail.com>
2023-10-05 19:03:53 +0300

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv, exit
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-else:
-    a = argv[1]
-    b = argv[3]
-    operator = argv[2]
-    if operator == '+':
-        res = add(int(a), int(b))
-        print("{} + {} = {}".format(a, b, res))
-    elif operator == '-':
-        res = sub(int(a), int(b))
-        print("{} - {} = {}".format(a, b, res))
-    elif operator == '*':
-        res = mul(int(a), int(b))
-        print("{} * {} = {}".format(a, b, res))
-    elif operator == '/':
-        res = div(int(a), int(b))
-        print("{} / {} = {}".format(a, b, res))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
+        if operator == '+':
+            res = add(a, b)
+            print("{} + {} = {}".format(a, b, res))
+        elif operator == '-':
+            res = sub(a, b)
+            print("{} - {} = {}".format(a, b, res))
+        elif operator == '*':
+            res = mul(a, b)
+            print("{} * {} = {}".format(a, b, res))
+        elif operator == '/':
+            res = div(a, b)
+            print("{} / {} = {}".format(a, b, res))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)

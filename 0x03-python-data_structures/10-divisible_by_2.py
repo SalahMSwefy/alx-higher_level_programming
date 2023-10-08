36969cd59ce48db1fd95bc9ea3e83dffd12c0566
SalahMSwefy <salahm.swefy@gmail.com>
2023-10-08 23:25:11 +0300

    0x03-python-data_structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     res = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            res[i] = True
+            res.append(True)
         else:
-            res[i] = False
+            res.append(False)
     return res

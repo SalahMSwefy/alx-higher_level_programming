baef8997715aa9503039dfecd059855bfb64e440
SalahMSwefy <salahm.swefy@gmail.com>
2023-10-12 02:18:51 +0300

    0x04-python-more_data_structuresgit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    score = 0
+    weight = 0
+    if not my_list:
+        return 0
+    else:
+        for i in my_list:
+            score += i[0] * i[1]
+            weight += i[1]
+    return score / weight

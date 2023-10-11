66a0e0cf306ab42f399e9b7bd2eacef3d8c2a27c
SalahMSwefy <salahm.swefy@gmail.com>
2023-10-12 01:54:55 +0300

    0x04-python-more_data_structuresgit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_score = -1
+    if not a_dictionary:
+        return None
+    else:
+        for key, value in a_dictionary.items():
+            if value > max_score:
+                max_score = res = value
+    return res

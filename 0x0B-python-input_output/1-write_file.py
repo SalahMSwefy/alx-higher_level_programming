c21cfff0781ff3275ca805eb1f16cb88c354991b
SalahMSwefy <salahm.swefy@gmali.com>
2023-11-08 17:20:52 +0200

    # 0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Write file"""
-    with open(filename, mode="w", encoding="UFT-8") as f:
+    with open(filename,"w") as f:
         return f.write(text)

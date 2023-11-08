097de9674d9d5a976b8ff3d174a2099299d748d7
SalahMSwefy <salahm.swefy@gmali.com>
2023-11-08 18:06:31 +0200

    # 0x0B-python-input_output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list, and then save them to a file"""
+import sys
+import os.path
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
+
+for i in sys.argv[1:]:
+    my_list.append(i)
+
+save_to_json_file(my_list, filename)

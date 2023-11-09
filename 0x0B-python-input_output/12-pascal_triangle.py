a578fe5ac0460a9464f3c858f0874aa40d9da89b
SalahMSwefy <salahm.swefy@gmali.com>
2023-11-09 03:13:10 +0200

    # 0x0B-python-input_output

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""pascale triangle"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal's triangle of n"""
+    res = [[1]]
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(temp) - 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res

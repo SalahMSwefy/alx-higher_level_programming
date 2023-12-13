c396cb07dda007773763336cad8dcdb1ca249943
SalahMSwefy <salahm.swefy@gmali.com>
2023-12-13 06:40:24 +0200

    # 0x0D-SQL_introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates a table second_table in the database hbtn_0c_0
 create table if not exists `second_table` (`id` int, `name` varchar(256), `score` int);
-insert into second_table (`id`, `name`, `score`) values (1, "John", 10)
-insert into second_table (`id`, `name`, `score`) values (2, "ALex", 3)
-insert into second_table (`id`, `name`, `score`) values (3, "Bob", 14)
-insert into second_table (`id`, `name`, `score`) values (4, "George", 8)
+insert into `second_table` (`id`, `name`, `score`) values (1, "John", 10)
+insert into `second_table` (`id`, `name`, `score`) values (2, "ALex", 3)
+insert into `second_table` (`id`, `name`, `score`) values (3, "Bob", 14)
+insert into `second_table` (`id`, `name`, `score`) values (4, "George", 8)

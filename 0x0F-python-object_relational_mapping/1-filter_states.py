9940ef74ba01804cea00c93f4fc5809bc1846794
SalahMSwefy <salahm.swefy@gmali.com>
2024-02-18 18:29:11 +0200

    # 0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         host='localhost', port=3306, charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`")
     rows = cur.fetchall()

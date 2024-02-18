31629cddf8e295f30a13af026f41b147b9e23791
SalahMSwefy <salahm.swefy@gmali.com>
2024-02-18 18:12:32 +0200

    # 0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+1-filter_states.py:
+script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()

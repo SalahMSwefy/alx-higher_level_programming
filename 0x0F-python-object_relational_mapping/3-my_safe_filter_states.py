af62bd28c025fddf714bb9266abed560f2898284
SalahMSwefy <salahm.swefy@gmali.com>
2024-02-18 18:38:16 +0200

    # 0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+3-my_safe_filter_states.py:
+script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password>
+<database name> <state name searched>
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         host='localhost', port=3306, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE `name` LIKE BINARY %s",
+                (argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()

e25060d25b3fe9a4cc0d9657496b59470eb925b9
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-04 18:41:40 +0200

    # 0x10-python-network_0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will take in a URL, send a GET request to the URL, and display the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"

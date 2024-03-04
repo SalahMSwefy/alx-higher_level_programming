49333ee1bfe413964a44c36accade07874677aa5
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-04 18:11:04 +0200

    # 0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # This script will print the size of the body of the response
 
-curl -si "$1" | grep "Content-Length" | cut -d " " -f 2
+curl -si "$1" | grep 'Content-Length' | cut -d " " -f 2

d391248b4a2fee6a0b5ed22d7f5594d8e6fc5c36
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-05 22:35:23 +0200

    # 0x11-python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
-    data = urllib.parse.urlencode({'email': email}.encode('utf-8'))
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))

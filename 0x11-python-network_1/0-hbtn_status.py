5ba55496f7f4239373a485d6d8af45bd32c95cb5
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-05 21:36:32 +0200

    # 0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib package"""
+"""Fetches https://alx-intranet.hbtn.io/status using urllib package"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
         html = resp.read()
     print("Body response:")
     print("\t- type: {}".format(type(html)))

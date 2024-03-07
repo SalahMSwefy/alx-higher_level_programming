5f9acded17c00f317cdadca20a8a4bdd801f08e0
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-07 02:26:35 +0200

    # 0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 '''
-Write a Python script that takes in a URL, 
-sends a request to the URL and displays the body of the response (decoded in utf-8).
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response
+(decoded in utf-8).
 '''
 import urllib.request
 from sys import argv

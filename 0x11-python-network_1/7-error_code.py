4b4d8d5d45166f38ce6ac352a271e5b4f459826b
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-07 03:02:48 +0200

    # 0x11-python-network_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     if r.status_code == 200:
         print(r.text)
     elif r.status_code >= 400:
-        print('Error code: '.format(r.status_code))
+        print('Error code: {}'.format(r.status_code))

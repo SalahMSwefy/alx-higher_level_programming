237f482718a96fd941efbf04097222dd8a80bd0c
SalahMSwefy <salahm.swefy@gmali.com>
2024-03-07 03:01:05 +0200

    # 0x11-python-network_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get(argv[1])
+    url = argv[1]
+    r = requests.get(url)
     if r.status_code == 200:
         print(r.text)
     elif r.status_code >= 400:

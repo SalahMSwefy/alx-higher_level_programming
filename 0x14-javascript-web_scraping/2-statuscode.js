85cc31105762494345b9d11a26e65ece5636ea1d
SalahMSwefy <salahm.swefy@gmali.com>
2024-04-24 15:50:37 +0200

    # 0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  else 
+    console.log('code: '+ response.statusCode);
+});

ff75028ca57bb8529a688e6340db6dbf092e3f16
SalahMSwefy <salahm.swefy@gmali.com>
2024-04-27 11:35:41 +0300

    # 0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const request = require('request');
+
+request({ url, method: 'GET' }, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else { 
+    const characters = JSON.parse(body).characters;
+    characters.forEach(character => {
+      request({ url: character, method: 'GET' }, (err, response, body) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        } else { console.log(JSON.parse(body).name); }
+      });
+    });
+   }
+});
